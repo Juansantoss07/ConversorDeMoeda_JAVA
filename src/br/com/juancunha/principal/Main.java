4150b1b0e5e8efab9f11b06880d137e1877732cc
Juansantoss07 <juansantosscunha@gmail.com>
2024-05-20 20:16:03 -0300

    first commit

---
+++ src/br/com/juancunha/principal/Main.java
@@ -0,0 +1,10 @@
+package br.com.juancunha.principal;
+
+import br.com.juancunha.menu.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menu();
+    }
+}
